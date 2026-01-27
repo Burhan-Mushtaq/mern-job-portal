59fa7047b7be4860a6124caef8010a361a6ef619
Burhan-Mushtaq <techbmr6@gmail.com>
2026-01-27 15:33:15 +0530

    Added user authentication with JWT

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("api/auth" , authRoutes)
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
+app.get("/", (req,res)=>{
+    res.send("Job Portal API Running");
+})
+
+app.listen(5000 , ()=> console.log("Server running"));
